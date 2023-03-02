1efd29225b8c9805710a65c288a1d2133e641181
libbyReeves <libby.reeves@hotmail.com>
2023-03-02 00:11:21 -0500

    added more hamburger functionality

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const lineOne = document.querySelector(".line-one");
 const lineTwo = document.querySelector(".line-two");
 const lineThree = document.querySelector(".line-three");
 const menuContainer = document.querySelector(".mobile-menu-container");
-// const wholePage = document.querySelector("body");
 
 hamburgerIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle("open");
@@ -13,8 +12,19 @@ hamburgerIcon.addEventListener('click', () => {
     lineTwo.classList.toggle("hamburger-animate");
     lineThree.classList.toggle("hamburger-animate");
     menuContainer.classList.toggle("open");
-    // wholePage.classList.toggle("open");
-})
+    
+});
+const anchorLinks = mobileMenu.querySelectorAll("a");
+for (let link = 0; link < anchorLinks.length; link++) {
+    anchorLinks[link].addEventListener('click', () => {
+        mobileMenu.classList.toggle("open");
+        lineOne.classList.toggle("hamburger-animate");
+        lineTwo.classList.toggle("hamburger-animate");
+        lineThree.classList.toggle("hamburger-animate");
+        menuContainer.classList.toggle("open");        
+    });
+
+}
 
 
 
@@ -23,3 +33,5 @@ let rotatingWords = document.querySelectorAll("rotate")
 rotatingWords.forEach( word => {
     let letters = word.textContent.split("");
 })
+
+console.log(mobileMenu.querySelectorAll("a"));
